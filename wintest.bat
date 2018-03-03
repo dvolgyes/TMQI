dc250dde30e304c0f6b2a67527ec1486257d4985
David Völgyes <david.volgyes@ieee.org>
2018-03-03 14:04:59 +0100

    wintests

--- wintest.bat
+++ wintest.bat
@@ -1,2 +1,2 @@
-"C:\\conda\\python.exe"  TMQI.py
-"C:\\conda\\python.exe"  TMQI.py data/test.float32 data/test_ldr.float32         -i float32 -W 396 -H 561 -g
+%CMD_IN_ENV% python TMQI.py
+%CMD_IN_ENV% python TMQI.py data/test.float32 data/test_ldr.float32         -i float32 -W 396 -H 561 -g
