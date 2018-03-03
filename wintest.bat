9d7992a25e76e807c813672364a5f59f81076c46
David Völgyes <david.volgyes@ieee.org>
2018-03-03 13:55:50 +0100

    another fix attempt

--- wintest.bat
+++ wintest.bat
@@ -1,2 +1,2 @@
-"%PYTHON%\\python.exe"  TMQI.py
-"%PYTHON%\\python.exe"  TMQI.py data/test.float32 data/test_ldr.float32         -i float32 -W 396 -H 561 -g
+"C:\\conda\\python.exe"  TMQI.py
+"C:\\conda\\python.exe"  TMQI.py data/test.float32 data/test_ldr.float32         -i float32 -W 396 -H 561 -g
